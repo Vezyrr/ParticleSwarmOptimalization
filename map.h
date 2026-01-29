0b81827c6070d625e5f371b6b8f12cd0209a1f70
Szymon Jakubiak <jakuszymo@gmail.com>
2026-01-29 13:42:22 +0100

    Header do wczytywania mapy z pliku/ zwalniania pamieci/ rzutowanie wartosci/ obsluga granic

---
+++ map.h
@@ -0,0 +1,21 @@
+
+typedef struct {
+    int width;          
+    int height;         
+    double *cells;      // Tablica 1D przechowująca wartości sygnału (spłaszczona 2D)
+} Map;
+
+
+ // Format pliku:
+ // W H
+// v1 v2 v3 
+
+Map* load_map_from_file(const char *filename);
+
+void free_map(Map *map);
+
+/**
+ * Pobiera wartość z mapy dla danych współrzędnych (double).
+ * Realizuje rzutowanie na int i obsługę granic.
+ */
+double get_map_value(const Map *map, double x, double y);
